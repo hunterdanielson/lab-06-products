3c533faa2dc1d70bd6da6b680cdf5e51251ae781
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-06 14:15:16 -0700

    added product data

---
+++ data/games.js
@@ -0,0 +1,67 @@
+const pokemonEmerald = {
+    id: 'pokemonEmerald',
+    name: 'Pokemon Emerald',
+    image: 'pokemonEmerald.jpg',
+    description: 'A classic pokemon game',
+    category: 'GBA',
+    price: 5.00,
+};
+const fireEmblem = {
+    id: 'fireEmblem',
+    name: 'Fire Emblem: Three houses',
+    image: 'fireEmblem.jpg',
+    description: 'Modern fire emblem game',
+    category: 'switch',
+    price: 40.00,
+};
+const zeldaMinishCap = {
+    id: 'zeldaMinishCap',
+    name: 'The Legend of Zelda: The Minish Cap',
+    image: 'zeldaMinish.jpg',
+    description: 'Zelda game where link is shrunk down to minature size',
+    category: 'GBA',
+    price: 4.00,
+};
+const zeldaBreath = {
+    id: 'zeldaBreath',
+    name: 'The Legend of Zelda: Breath of the Wild',
+    image: 'zeldaBreath.jpeg',
+    description: 'Best selling zelda game, staple of any switch',
+    category: 'switch',
+    price: 45.00,
+};
+const pokemonSword = {
+    id: 'pokemonSword',
+    name: 'Pokemon Sword',
+    image: 'pokemonSword.jpg',
+    description: 'New generation pokemon game, sister to pokemon shield',
+    category: 'switch',
+    price: 40.00,
+};
+const marioKart8 = {
+    id: 'marioKart8',
+    name: 'Mario Kart 8',
+    image: 'marioKart8.jpeg',
+    description: 'Classic mario kart, with some modern twists',
+    category: 'switch',
+    price: 30.00,
+};
+const animalCrossing = {
+    id: 'animalCrossing',
+    name: 'Animal Crossing: New Horizons',
+    image: 'animalCrossing.jpg',
+    description: 'Animal Crossing on a modern console, lets you visit friends islands',
+    category: 'switch',
+    price: .76,
+    cost: .75
+};
+const games = [
+    pokemonEmerald,
+    fireEmblem,
+    zeldaMinishCap,
+    zeldaBreath,
+    pokemonSword,
+    marioKart8,
+    animalCrossing
+];
+export default games;
