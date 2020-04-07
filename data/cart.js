e30cde87528da2575ad45a1e0b069b6e04b17ac6
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-07 13:58:07 -0700

    add cart data

---
+++ data/cart.js
@@ -0,0 +1,18 @@
+const cart = [{
+    id: 'fireEmblem',
+    quantity: 1
+}, {
+    id: 'zeldaBreath',
+    quantity: 1
+}, {
+    id: 'pokemonSword',
+    quantity: 2
+}, {
+    id: 'marioKart8',
+    quantity: 1
+}, {
+    id: 'animalCrossing',
+    quantity: 1
+}];
+
+export default cart;
